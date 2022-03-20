e5f44aeade5f93defe7cc3f893febfe903f61ecc
blur <damienhebert710@gmail.com>
2022-03-20 11:55:27 -0500

    latestchanges

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -63,24 +63,24 @@
 
 // Generator functions - http://www.net-comber.com/charset.html
 
-function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+// function getRandomLower() {
+//     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
 
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// function getRandomUpper() {
+//     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 
-}
+// }
 
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// function getRandomNumber() {
+//     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 
-}
+// }
 
-function getRandomSymbol() {
-    const symbols = '!@#$^%&*()_+/?.,';
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
+// function getRandomSymbol() {
+//     const symbols = '!@#$^%&*()_+/?.,';
+//     return symbols[Math.floor(Math.random() * symbols.length)];
+// }
 
 
 
